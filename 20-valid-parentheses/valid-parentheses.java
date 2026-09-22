26da9146ad873d5ebbfb9394632bc54fa06f5fe8
riteshkumarp569-stack <riteshkumarp569@gmail.com>
2026-09-23 03:34:31 +0530

    Time: 4 ms (36.76%) | Memory: 43.6 MB (9.90%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+                st.push(s.charAt(i));
+                continue;
+            }
+            if(st.empty()){
+                return false;
+            }
+            if(s.charAt(i)==')' && st.peek()=='(' || (s.charAt(i)==']' &&st.peek()=='[') || (s.charAt(i)=='}' && st.peek()=='{' )){
+                st.pop();
+            }else{
+                return false;
+            }
+          
+
+        }
+        if(!st.empty()){
+            return false;
+        }
+return true;
+    }
+}
