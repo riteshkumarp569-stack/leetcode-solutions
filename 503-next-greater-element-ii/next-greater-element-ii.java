8a319f6a3d5d40b25eb210348d0f2eb6ef1e3f90
riteshkumarp569-stack <riteshkumarp569@gmail.com>
2026-09-24 03:01:06 +0530

    Time: 13 ms (69.14%) | Memory: 48.8 MB (13.12%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+       int n=nums.length;
+        int[] arr=new int[n];
+       
+        Stack<Integer> st = new Stack<>();
+       for(int i=n-2;i>=0;i--){
+        st.push(nums[i]);
+       }
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.peek()<=nums[i]){
+                st.pop();
+            }
+             if(st.empty()){
+                    arr[i]=-1;
+                }else{
+                    arr[i]=st.peek();
+                }
+            st.push(nums[i]);
+        } 
+        return arr;
+    }
+}
